800c1c03af97d9794e276c9f42b9529d068ea937
pyo-sh <pygosky@gmail.com>
2022-08-02 19:55:27 +0900

    feat :sparkles: Navigator 컴포넌트에서 드래그 기능 구현

--- packages/client/src/components/navigator/index.tsx
+++ packages/client/src/components/navigator/index.tsx
@@ -10,12 +10,55 @@ import {
 const dummyData = [1, 2, 3, 4, 5];
 
 const Navigator: React.FC = ({}) => {
+  const $navList = useRef<HTMLUListElement>(null);
+  const isClickPrevented = useRef<boolean>(true);
+  const isDragging = useRef<boolean>(false);
+
+  const onMouseDownList: React.MouseEventHandler<HTMLUListElement> = () => {
+    isClickPrevented.current = false;
+  };
+
+  const onMouseMoveList: React.MouseEventHandler<HTMLUListElement> = ({ movementX }) => {
+    // Detect Drag
+    if (!isClickPrevented.current) {
+      isDragging.current = true;
+    }
+    // Prevent Click
+    isClickPrevented.current = true;
+    // Dragging
+    if (isDragging.current && $navList.current) {
+      $navList.current.scrollLeft -= movementX * 2;
+    }
+  };
+
+  const onMouseUpList: React.MouseEventHandler<HTMLUListElement> = (e) => {
+    if (isDragging.current) {
+      console.log("drag");
+    } else {
+      console.log("click");
+    }
+    isClickPrevented.current = true;
+    isDragging.current = false;
+  };
+
+  const onMouseLeaveList: React.MouseEventHandler<HTMLUListElement> = () => {
+    isClickPrevented.current = true;
+    isDragging.current = false;
+  };
+
   return (
     <header className={navContainerStyle}>
       <h1 className={titleStyle}>주문</h1>
       <nav className={navStyle}>
         <button className={controllerStyle}></button>
-        <ul className={navListStyle}>
+        <ul
+          ref={$navList}
+          className={navListStyle}
+          onMouseDown={onMouseDownList}
+          onMouseMove={onMouseMoveList}
+          onMouseUp={onMouseUpList}
+          onMouseLeave={onMouseLeaveList}
+        >
           {dummyData.map((item) => (
             <li key={`nav-${item}`}>{item}</li>
           ))}
