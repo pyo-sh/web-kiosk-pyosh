16869589cda8f5d0dd04c5eb44d31ea6cc2b4b35
pyo-sh <pygosky@gmail.com>
2022-08-02 11:06:34 +0900

    style :lipstick: 여러 공통 스타일들을 미리 선언 #5

--- packages/client/src/components/custom/Modal.style.ts
+++ packages/client/src/components/custom/Modal.style.ts
@@ -1,4 +1,5 @@
 import { css } from "@emotion/css";
+import { BOX_SHADOW } from "../../constants/style";
 
 interface modalStylePropType {
   zIndex?: number;
@@ -19,6 +20,7 @@ export const backgroundStyle = ({ zIndex }: modalStylePropType) => css`
 `;
 
 export const modalStyle = ({ zIndex }: modalStylePropType) => css`
+  ${BOX_SHADOW}
   min-width: 400px;
   min-height: 100px;
   padding: 5px;
@@ -31,6 +33,4 @@ export const modalStyle = ({ zIndex }: modalStylePropType) => css`
   opacity: 1;
 
   background-color: white;
-  border-radius: 10px;
-  box-shadow: 0px 0px 4px rgba(204, 204, 204, 0.5), 0px 2px 4px rgba(0, 0, 0, 0.25);
 `;
--- packages/client/src/constants/style.ts
+++ packages/client/src/constants/style.ts
@@ -1,5 +1,9 @@
-export const Color = Object.freeze({
+import { css } from "@emotion/css";
+
+export const COLOR = Object.freeze({
   primary: "#2AC1BC",
+  primary2: "#A0E1E0",
+  primary3: "#4CB8B8",
   titleActive: "#1E2019",
   body: "#626666",
   label: "#8D9393",
@@ -8,5 +12,59 @@ export const Color = Object.freeze({
   background: "#F5F5F5",
   offWhite: "#FCFCFC",
   error: "#F45452",
+  lightError: "#FFD4D3",
+  darkError: "#CD6766",
   darkGreen: "#394032",
 });
+
+export const INPUT_STYLE = css`
+  padding: 0;
+  margin: 0;
+  outline: none;
+  color: ${COLOR.titleActive};
+  border: 1px solid ${COLOR.line};
+  &:focus {
+    border: 1px solid ${COLOR.primary};
+  }
+`;
+
+export const BTN_STYLE = css`
+  padding: 0;
+  margin: 0;
+  background-color: ${COLOR.primary};
+  color: ${COLOR.offWhite};
+  border: 2px solid transparent;
+  cursor: pointer;
+  &:hover {
+    background-color: ${COLOR.primary3};
+  }
+  &:focus {
+    border: 2px solid ${COLOR.primary2};
+  }
+  &:disabled {
+    background-color: ${COLOR.primary2};
+  }
+`;
+
+export const CANCEL_BTN_STYLE = css`
+  padding: 0;
+  margin: 0;
+  background-color: ${COLOR.error};
+  color: ${COLOR.offWhite};
+  border: 2px solid transparent;
+  cursor: pointer;
+  &:hover {
+    background-color: ${COLOR.darkError};
+  }
+  &:focus {
+    border: 2px solid ${COLOR.lightError};
+  }
+  &:disabled {
+    background-color: ${COLOR.lightError};
+  }
+`;
+
+export const BOX_SHADOW = css`
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
+`;
