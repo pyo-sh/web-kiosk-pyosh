0126e14fda3cc33b4304e8991cafb7209e4e1928
pyo-sh <pygosky@gmail.com>
2022-08-02 16:04:25 +0900

    style :lipstick: 키오스크 외관 & 태블릿 이하의 길이 유지 스타일 #7

--- packages/client/src/Global.style.ts
+++ packages/client/src/Global.style.ts
@@ -1,10 +1,12 @@
 import { injectGlobal } from "@emotion/css";
+import { COLOR } from "./constants/style";
 
 injectGlobal`
     * {
         box-sizing: border-box;
     }
-    html, body, #root {
+
+    html, body, #root{
         width: 100%;
         height: 100%;
         margin: 0;
@@ -12,11 +14,27 @@ injectGlobal`
         font-family: 'Noto Sans KR';
         font-style: normal; 
     }
+
     @font-face {
         font-family: "BMDOHYEON";
         src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMDOHYEON.woff")
             format("woff");
         font-weight: normal;
         font-style: normal;
+    }
+
+    body {
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        background-color: ${COLOR.titleActive};
+    }
+
+    #root {
+        max-width: 1280px;
+
+        border: 1px solid black;
+        border-radius: 10px;
+        background-color: ${COLOR.white};
     }
 `;
