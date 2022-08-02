5f30e6b89831c11742d8150c7f2664b270791000
pyo-sh <pygosky@gmail.com>
2022-08-02 10:57:46 +0900

    style :lipstick: Color 변수 선언

---
+++ packages/client/src/constants/style.ts
@@ -0,0 +1,12 @@
+export const Color = Object.freeze({
+  primary: "#2AC1BC",
+  titleActive: "#1E2019",
+  body: "#626666",
+  label: "#8D9393",
+  placeholder: "#C1C5C5",
+  line: "#CCD3D3",
+  background: "#F5F5F5",
+  offWhite: "#FCFCFC",
+  error: "#F45452",
+  darkGreen: "#394032",
+});
