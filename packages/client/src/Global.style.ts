54616bc407fcb63a2afba522f7f9415445d5e1f1
pyo-sh <pygosky@gmail.com>
2022-08-02 12:38:10 +0900

    style :lipstick: BMDOHYEON font 사용 #5

--- packages/client/src/Global.style.ts
+++ packages/client/src/Global.style.ts
@@ -9,5 +9,14 @@ injectGlobal`
         height: 100%;
         margin: 0;
         padding: 0;
+        font-family: 'Noto Sans KR';
+        font-style: normal; 
+    }
+    @font-face {
+        font-family: "BMDOHYEON";
+        src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMDOHYEON.woff")
+            format("woff");
+        font-weight: normal;
+        font-style: normal;
     }
 `;
