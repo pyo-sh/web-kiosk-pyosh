10ce6e0623d463c25a6d374fcb5b849d9e5c067f
pyo-sh <pygosky@gmail.com>
2022-08-03 19:46:22 +0900

    init :tada: TypeORM 을 NestJS에 적용하기 #11

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { validateEnv } from "./configs/env.validation";
+import { validateEnv, Environment } from "./configs/env.validation";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnv,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get("DB_HOST"),
+        port: +configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASS"),
+        database: configService.get("DB_NAME"),
+        entities: [],
+        synchronize: configService.get("NODE_ENV") === Environment.Development,
+      }),
     }),
   ],
   controllers: [AppController],
