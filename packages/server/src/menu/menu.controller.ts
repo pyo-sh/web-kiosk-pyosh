606d7ce8bb7a9ff1469d86b5879ca99455f83dcb
pyo-sh <pygosky@gmail.com>
2022-08-04 16:52:29 +0900

    feat :sparkles: Menu CRUD API 및 Swagger 적용 #13

--- packages/server/src/menu/menu.controller.ts
+++ packages/server/src/menu/menu.controller.ts
@@ -1,34 +1,65 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { MenuService } from './menu.service';
-import { CreateMenuDto } from './dto/create-menu.dto';
-import { UpdateMenuDto } from './dto/update-menu.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus } from "@nestjs/common";
+import { MenuService } from "./menu.service";
+import { CreateMenuDto } from "./dto/create-menu.dto";
+import { UpdateMenuDto } from "./dto/update-menu.dto";
+import { Response } from "express";
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from "@nestjs/swagger";
 
-@Controller('menu')
+@ApiTags("메뉴 API")
+@Controller("menu")
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
+  @ApiOperation({ summary: "메뉴 생성 API", description: "메뉴 생성하고 정보를 반환한다" })
+  @ApiCreatedResponse({ description: "생성 성공" })
   @Post()
-  create(@Body() createMenuDto: CreateMenuDto) {
-    return this.menuService.create(createMenuDto);
+  async create(@Body() createMenuDto: CreateMenuDto, @Res() res: Response) {
+    const menu = await this.menuService.create(createMenuDto);
+    return res.status(HttpStatus.CREATED).json({ menu });
   }
 
+  @ApiOperation({ summary: "전체 메뉴 읽기 API", description: "전체 메뉴를 읽어서 반환한다" })
+  @ApiOkResponse({ description: "읽기 성공" })
   @Get()
-  findAll() {
-    return this.menuService.findAll();
+  async findAll(@Res() res: Response) {
+    const menus = await this.menuService.findAll();
+    return res.status(HttpStatus.OK).json({ menus });
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.menuService.findOne(+id);
+  @ApiOperation({ summary: "메뉴 읽기 API", description: "id에 해당하는 메뉴를 반환한다" })
+  @ApiOkResponse({ description: "읽기 성공" })
+  @Get(":id")
+  async findOne(@Param("id") id: string, @Res() res: Response) {
+    const menu = await this.menuService.findOne(+id);
+    return res.status(HttpStatus.OK).json({ menu });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
-    return this.menuService.update(+id, updateMenuDto);
+  @ApiOperation({
+    summary: "메뉴 수정 API",
+    description: "id에 해당하는 메뉴를 정보와 같이 바꾸고 반환한다",
+  })
+  @ApiOkResponse({ description: "수정 성공" })
+  @Patch(":id")
+  async update(
+    @Param("id") id: string,
+    @Body() updateMenuDto: UpdateMenuDto,
+    @Res() res: Response,
+  ) {
+    const updatedMenu = await this.menuService.update(+id, updateMenuDto);
+    return res.status(HttpStatus.OK).json({ menu: updatedMenu });
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.menuService.remove(+id);
+  @ApiOperation({ summary: "메뉴 삭제 API", description: "id에 해당하는 메뉴를 삭제합니다" })
+  @ApiNoContentResponse({ description: "삭제 성공" })
+  @Delete(":id")
+  async remove(@Param("id") id: string, @Res() res: Response) {
+    await this.menuService.remove(+id);
+    return res.status(HttpStatus.NO_CONTENT).end();
   }
 }
--- packages/server/src/menu/menu.service.ts
+++ packages/server/src/menu/menu.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, Repository } from "typeorm";
 import { CreateMenuDto } from "./dto/create-menu.dto";
 import { UpdateMenuDto } from "./dto/update-menu.dto";
 import { Menu } from "./entities/menu.entity";
@@ -11,8 +11,8 @@ export class MenuService {
     this.menuRepository = menuRepository;
   }
 
-  create(createMenuDto: CreateMenuDto) {
-    return this.menuRepository.create(createMenuDto);
+  create(createMenuDto: CreateMenuDto): Promise<Menu> {
+    return this.menuRepository.save(createMenuDto);
   }
 
   findAll(): Promise<Menu[]> {
@@ -23,11 +23,13 @@ export class MenuService {
     return this.menuRepository.findOneBy({ id });
   }
 
-  update(id: number, updateMenuDto: UpdateMenuDto) {
-    return this.menuRepository.update(id, updateMenuDto);
+  async update(id: number, updateMenuDto: UpdateMenuDto): Promise<Menu> {
+    const pureMenu = await this.menuRepository.findOneBy({ id });
+    await this.menuRepository.update(id, updateMenuDto);
+    return { ...pureMenu, ...updateMenuDto };
   }
 
-  remove(id: number) {
+  remove(id: number): Promise<DeleteResult> {
     return this.menuRepository.delete({ id });
   }
 }
