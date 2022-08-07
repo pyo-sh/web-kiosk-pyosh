ff670faddf2c3cba08e0292c0f7570d637f5ea99
pyo-sh <pygosky@gmail.com>
2022-08-07 21:49:48 +0900

    feat :sparkles: axios default 설정 #27

---
+++ client/src/apis/index.ts
@@ -0,0 +1,18 @@
+import { SERVER_URL } from "@constants/envs";
+import axios, { AxiosResponse } from "axios";
+
+function initAxios() {
+  axios.defaults.baseURL = SERVER_URL;
+  axios.interceptors.response.use(
+    (res: AxiosResponse<any, any>) => res,
+    async (error) => {
+      const {
+        response: { status },
+      } = error;
+      // TODO : Error Handler
+      return Promise.reject(error);
+    },
+  );
+}
+
+export default initAxios;
