ebe0b2aca48fbbeb6759ae79836727efd018e1ab
pyo-sh <pygosky@gmail.com>
2022-08-09 19:28:06 +0900

    fix :wrench: Context API 를 사용해서 현재 media match 전달 #33

--- client/src/pages/Order.tsx
+++ client/src/pages/Order.tsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ContainerRowDiv, ContainerColDiv } from "./Order.style";
-import useMediaQuery from "@hooks/useMediaQuery";
-import { ScreenQuery } from "@constants/screen";
 import Product from "@components/product";
 import Cart from "@components/cart";
+import { MediaContext } from "@hooks/useMediaQuery";
 
 const Order: React.FC = () => {
-  const isMobile = useMediaQuery(ScreenQuery.mobile);
+  const isMobile = useContext(MediaContext);
   const ContainerDiv = isMobile ? ContainerColDiv : ContainerRowDiv;
 
   return (
     <ContainerDiv>
       <Product />
-      <Cart isMobile={isMobile} />
+      <Cart />
     </ContainerDiv>
   );
 };
